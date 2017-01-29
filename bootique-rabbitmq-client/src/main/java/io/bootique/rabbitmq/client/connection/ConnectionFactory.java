cf7dc09c6f79e5bb90da48abcc4c4c0f9596fbe6
realyelias <realyelias@gmail.com>
2017-01-29 12:08:20 +0300

    connection factory error handling

--- bootique-rabbitmq-client/src/main/java/io/bootique/rabbitmq/client/connection/ConnectionFactory.java
+++ bootique-rabbitmq-client/src/main/java/io/bootique/rabbitmq/client/connection/ConnectionFactory.java
@@ -2,6 +2,8 @@ package io.bootique.rabbitmq.client.connection;
 
 import com.rabbitmq.client.Connection;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.Map;
@@ -9,15 +11,28 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class ConnectionFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionFactory.class);
+
     private Map<String, ConnectionConfig> configMap;
     private final ConcurrentMap<String, Connection> connectionList = new ConcurrentHashMap<>();
+    private volatile Boolean shutdown;
+    private final Object lock = new Object();
 
     public ConnectionFactory(Map<String, ConnectionConfig> configMap) {
         this.configMap = configMap;
     }
 
     public void shutdown() {
-        connectionList.values().forEach(IOUtils::closeQuietly);
+        shutdown = true;
+        LOGGER.info("Shutting down RabbitMQ service...");
+        synchronized (lock) {
+            LOGGER.info("Closing Rabbit MQ broker connections");
+            connectionList.values()
+                    .stream()
+                    .filter(Connection::isOpen)
+                    .forEach(IOUtils::closeQuietly);
+            connectionList.clear();
+        }
     }
 
     public Collection allNames() {
@@ -25,12 +40,24 @@ public class ConnectionFactory {
     }
 
     public Connection forName(String connectionName) {
-        return connectionList.computeIfAbsent(connectionName, this::createConnection);
+        if (shutdown) {
+            throw new IllegalStateException("Can't get connection -- Rabbit MQ service is shut down");
+        }
+        synchronized (lock) {
+            return isConnectionValid(connectionName) ? connectionList.get(connectionName) : createConnection(connectionName);
+        }
     }
 
     private Connection createConnection(String connectionName) {
-        return configMap.computeIfAbsent(connectionName, name -> {
+        Connection connection = configMap.computeIfAbsent(connectionName, name -> {
             throw new IllegalStateException("No configuration present for Connection named '" + name + "'");
         }).createConnection();
+
+        connectionList.put(connectionName, connection);
+        return connection;
+    }
+
+    private boolean isConnectionValid(String connectionName) {
+        return connectionList.containsKey(connectionName) && connectionList.get(connectionName).isOpen();
     }
 }
